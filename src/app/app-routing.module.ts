9a3ab6b681f61b72e9d058854721f6a5026b4ac2
enzookita <enzookita@hotmail.com>
2021-12-27 17:23:56 -0400

    2555

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimallistarComponent } from './animallistar/animallistar.component';
 import { CompradorlistarComponent } from './compradorlistar/compradorlistar.component';
-<<<<<<< HEAD
 import { VeterinariolistarComponent } from './veterinariolistar/veterinariolistar.component';
-=======
 import { VendedorComponent } from './vendedorlistar/vendedor.component';
->>>>>>> d24236e2f93aa23f62dfe496b9e7f294b0b2b090
+
 
 const routes: Routes = [
   {path: "comprador", component:CompradorlistarComponent},
   {path: "animal", component: AnimallistarComponent},
-<<<<<<< HEAD
-  {path: "veterinario", component: VeterinariolistarComponent}
-=======
+  {path: "veterinario", component: VeterinariolistarComponent},
   {path: "vendedor", component: VendedorComponent}
->>>>>>> d24236e2f93aa23f62dfe496b9e7f294b0b2b090
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompradorlistarComponent } from './compradorlistar/compradorlistar.component';
 import { AnimallistarComponent } from './animallistar/animallistar.component';
-<<<<<<< HEAD
 import { VeterinariolistarComponent } from './veterinariolistar/veterinariolistar.component';
-=======
 import { VendedorComponent } from './vendedorlistar/vendedor.component';
->>>>>>> d24236e2f93aa23f62dfe496b9e7f294b0b2b090
 
 @NgModule({
   declarations: [
     AppComponent,
     CompradorlistarComponent,
     AnimallistarComponent,
-<<<<<<< HEAD
-    VeterinariolistarComponent
-=======
+    VeterinariolistarComponent,
     VendedorComponent
->>>>>>> d24236e2f93aa23f62dfe496b9e7f294b0b2b090
   ],
   imports: [
     BrowserModule,
