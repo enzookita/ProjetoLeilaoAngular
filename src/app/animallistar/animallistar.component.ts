fde5b09b95646e2313010671e0216ebdc3cee684
enzookita <eokita@itelinfo.com>
2021-12-27 16:50:22 -0400

    modelAnimal

--- src/app/animallistar/animallistar.component.ts
+++ src/app/animallistar/animallistar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../model/animal';
+import { AnimalService } from '../services/animal.service';
 
 @Component({
   selector: 'app-animallistar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimallistarComponent implements OnInit {
 
-  constructor() { }
+  animales: Animal[] = [];
+
+  constructor(private servicoAnimal: AnimalService) { }
 
   ngOnInit(): void {
+
+    this.servicoAnimal.listarComprador().subscribe(
+      data => {this.animales = data}
+      
+    )
   }
 
 }
