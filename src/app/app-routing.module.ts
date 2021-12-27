d24236e2f93aa23f62dfe496b9e7f294b0b2b090
narciza-fialho <narciza.fialho@hotmail.com>
2021-12-27 17:07:05 -0400

    add rota vendedor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimallistarComponent } from './animallistar/animallistar.component';
 import { CompradorlistarComponent } from './compradorlistar/compradorlistar.component';
+import { VendedorComponent } from './vendedorlistar/vendedor.component';
 
 const routes: Routes = [
   {path: "comprador", component:CompradorlistarComponent},
-  {path: "animal", component: AnimallistarComponent}
+  {path: "animal", component: AnimallistarComponent},
+  {path: "vendedor", component: VendedorComponent}
 ];
 
 @NgModule({
