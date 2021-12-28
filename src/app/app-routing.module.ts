5cbd393d391908ee40aeb233614e96afa9b30406
ViniciusPaulaSouza <viniciuspaulasouza@gmail.com>
2021-12-28 09:10:22 -0400

    Adicionando leilao no app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AnimallistarComponent } from './animallistar/animallistar.component';
 import { CompradorlistarComponent } from './compradorlistar/compradorlistar.component';
 import { VeterinariolistarComponent } from './veterinariolistar/veterinariolistar.component';
 import { VendedorComponent } from './vendedorlistar/vendedor.component';
+import { LeilaolistarComponent } from './leilaolistar/leilaolistar.component';
 
 
 const routes: Routes = [
   {path: "comprador", component:CompradorlistarComponent},
   {path: "animal", component: AnimallistarComponent},
   {path: "veterinario", component: VeterinariolistarComponent},
-  {path: "vendedor", component: VendedorComponent}
+  {path: "vendedor", component: VendedorComponent},
+  {path: "leilao", component: LeilaolistarComponent}
 ];
 
 @NgModule({
